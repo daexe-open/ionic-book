67e81cf1e202819977e9b066d77c2a594e2c7072
Max Lynch <max@drifty.com>
2014-05-12 10:44:19 -0500

    Changed names

--- js/angular/directive/popover.js
+++ js/angular/directive/popover.js
@@ -5,27 +5,27 @@
  * @restrict E
  * @codepen saoBG
  * @description
- * The popdown directive makes it easy to add a button item with a dropdown list of options.
+ * The popover directive makes it easy to add a button item with a dropdown list of options.
  *
  * @usage
  * ```html
- * <ion-popdown>
+ * <ion-popover>
  *   <ion-item>Item 1</ion-item>
- * </ion-popdown>
+ * </ion-popover>
  * ```
  */
 IonicModule
-.directive('ionPopdown', ['$document', function($document) {
+.directive('ionPopover', ['$document', function($document) {
   return {
     restrict: 'A',
     compile: function(element, attr) {
-      element.addClass('popdown')
+      element.addClass('popover')
 
       var docClose = function(e) {
         if(e.type === 'keyup' && e.which == 27) {
-          element.removeClass('popdown-active');
+          element.removeClass('popover-active');
         } else if(e.type !== 'keyup') { 
-          element.removeClass('popdown-active');
+          element.removeClass('popover-active');
         }
 
         $document.off('click', docClose);
@@ -33,15 +33,15 @@ IonicModule
       };
 
       var open = function() {
-        element.addClass('popdown-active');
+        element.addClass('popover-active');
       };
 
       var close = function() {
-        element.removeClass('popdown-active');
+        element.removeClass('popover-active');
       };
 
       element.on('click', function(e) {
-        if(element.hasClass('popdown-active')) {
+        if(element.hasClass('popover-active')) {
           close();
           $document.off('click', docClose);
           $document.off('keyup', docClose);
